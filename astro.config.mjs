6020aef420496ff5d0f4c5fff7e511296c4774e7
Abdoulaye Guene <abdoulaye.guene92@gmail.com>
2024-02-20 11:21:45 +0000

    ♻️config google analytic (Détails du flux Web)

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,6 +30,15 @@ export default defineConfig({
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
   output: 'static',
+  
+  plugins: [
+    [
+      'astro-plugin-google-analytics',
+      {
+        id: 'G-TT2H971V99', // Remplacez par votre ID de suivi Google Analytics
+      },
+    ],
+  ],
 
   integrations: [
     tailwind({
